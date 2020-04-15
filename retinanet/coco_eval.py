31a731611aef7306d1a36cb3b9dd40407476e9d0
李張誌 <kasoarcat@gmail.com>
2020-04-15 11:38:01 +0800

    no message

--- retinanet/coco_eval.py
+++ retinanet/coco_eval.py
@@ -4,7 +4,7 @@ import json
 import torch
 from torch.utils.data import Subset
 
-def evaluate_coco(dataset, model, type, threshold=0.05):
+def evaluate_coco(dataset, model, _type, threshold=0.05):
     model.eval()
 
     with torch.no_grad():
@@ -83,7 +83,7 @@ def evaluate_coco(dataset, model, type, threshold=0.05):
             JSON_PATH = '/kaggle/working/' + JSON_PATH
         json.dump(results, open(JSON_PATH.format(name), 'w'), indent=4)
 
-        if type != '':
+        if _type != '':
             # load results in COCO evaluation tool
             coco_true = dataset.coco
             coco_pred = coco_true.loadRes('{}_bbox_results.json'.format(name))
